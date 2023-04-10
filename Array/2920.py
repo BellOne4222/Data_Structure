5f9308050b88cd704dfc715c0e33f839bd07cee5
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-04-10 14:33:36 +0900

    4/10

--- Array/2920.py
+++ Array/2920.py
@@ -1,2 +1,11 @@
 N = int(input())
-print(N)
+song = list(map(int, input().split()))
+a_song = [1,2,3,4,5,6,7,8]
+d_song = sorted(a_song, reverse=True)
+
+if song == a_song:
+    print("ascending")
+elif song == d_song:
+    print("descending")
+else:
+    print("mixed")
